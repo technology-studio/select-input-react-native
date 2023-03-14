b571393bf26cb03d2457f4a701d7935dafd62200
Erik Slovak <erik.slovak@technologystudio.sk>
2023-03-14 18:43:57 +0100

    test: add and update test configs

--- jest.config.js
+++ jest.config.js
@@ -9,28 +9,34 @@ const { compilerOptions } = require('./tsconfig.json');
 
 const { defaults } = require('jest-config');
 
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'react-native',
   testEnvironment: 'node',
   testMatch: [
-    '<rootDir>/__tests__/Tests/**/?(*.)(spec|test).ts'
+    '<rootDir>/__tests__/Tests/**/?(*.)(spec|test).ts',
+    '<rootDir>/src/components/**/__tests__/?(*.)(spec|test).(ts|tsx)'
   ],
   transformIgnorePatterns: [
-    '/node_modules/(?!@txo).+\\.js$'
+    // '/node_modules/(?!@txo).+\\.js$', // NOTE: add when needed
+    'node_modules/(?!(react-native|@react-native\/polyfills)/)'
   ],
   testPathIgnorePatterns: [
     '/node_modules/'
   ],
   setupFiles: [
-    '<rootDir>/__tests__/Setup.ts'
+    '<rootDir>/__tests__/Setup.ts',
+    '<rootDir>/node_modules/react-native/jest/setup.js'
   ],
   moduleFileExtensions: [
     ...defaults.moduleFileExtensions,
   ],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: './tsconfig.json'
+    '^.+\\.(js)$': 'babel-jest',
+    '^.+.tsx?$': ['ts-jest', {
+      tsconfig: './tsconfig-spec.json'
     }]
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , { prefix: '<rootDir>/' } ),
 }
