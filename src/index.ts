62e12abc23fa3032f77eea6bd102ba2050b5bf33
Erik Slovak <erik.slovak@technologystudio.sk>
2023-03-16 09:43:18 +0100

    chore: fix eslint issue

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { Platform } from 'react-native'
 const SelectInput = Platform.select({
   ios: () => require('./components/SelectInput.ios'),
   android: () => require('./components/SelectInput.android'),
-})();
+})?.()
 
 export default SelectInput.default
