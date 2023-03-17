259ee2ac7caf32e535dc3328b42a0d178844c9fd
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2023-03-17 08:49:48 +0100

    refactor: migrate default to named export/import

--- src/components/CustomKeyboard/CustomKeyboard.tsx
+++ src/components/CustomKeyboard/CustomKeyboard.tsx
@@ -18,7 +18,7 @@ type Props = {
   children: JSX.Element | null,
 }
 
-class CustomKeyboard extends Component<Props> {
+export class CustomKeyboard extends Component<Props> {
   state = {
     width: Dimensions.get('window').width,
   }
@@ -90,5 +90,3 @@ class CustomKeyboard extends Component<Props> {
     )
   }
 }
-
-export default CustomKeyboard
